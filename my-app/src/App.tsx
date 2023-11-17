bb7bc7dc07f2f4d5d45a8812166296e556e2e2ca
Tran-Dinh-Dat <45207367+Tran-Dinh-Dat@users.noreply.github.com>
2023-11-18 02:28:08 +0700

    List

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -4,14 +4,23 @@ import './App.css';
 import Button from './Button';
 import Greeter from './Greeter';
 import Hello from './Hello';
+import FruitList from './FruitList';
 
 function App() {
+  const data = [
+    { id: 1, name: 'apple' },
+    { id: 2, name: 'orange' },
+    { id: 3, name: 'blueberry' },
+    { id: 4, name: 'banana' },
+    { id: 5, name: 'kiwi' },
+  ];
   return (
     <div className="App">
       <Button />
       <div>
         <Greeter first="Dat" last='Tran Dinh'/>
         <Hello name='Dat' enthusiasmLevel={1}/>
+        <FruitList fruits={data}/>
       </div>
     </div>
   );
